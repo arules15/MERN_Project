972f6a98593cf5986cc154534dbcc5c96645455c
arules15 <arules14@gmail.com>
2018-08-12 01:08:53 -0400

    August 11th edits

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+//Post model
+const Post = require("../../models/Post");
+
+//Validation
+const validatePostInput = require("../../validation/post");
 
 // @route   GET api/posts/test
 // @desc    Tests post route
 // @access  Public route
 router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
+
+// @route   GET api/posts
+// @desc    Create post
+// @access  Private route
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    //Check Validation
+    if (!isValid) {
+      //If any errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -93,7 +93,7 @@ router.post("/login", (req, res) => {
         jwt.sign(
           payload,
           keys.secretOrKey,
-          { expiresIn: 3600 },
+          { expiresIn: 86400 },
           (err, token) => {
             res.json({
               success: true,
