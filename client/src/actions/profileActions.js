b8a2e9540940396de55d027160682ac0f98d1597
arules15 <arules14@gmail.com>
2018-12-27 17:55:16 -0500

    Starting profile interface

---
+++ client/src/actions/profileActions.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+import {
+  GET_PROFILE,
+  PROFILE_LOADING,
+  GET_ERRORS,
+  CLEAR_CURRENT_PROFILE
+} from "./types";
+
+//Get Current profile
+export const getCurrentProfile = () => dispatch => {
+  //call the profile loading function, in our reducer this will cause the profile loading circle to
+  //appear until the type has changed to GET_PROFILE , since the type of setProfileLoading is PROFILE_LOADING
+  //(refer to profilereducer to see what affect this has)
+  dispatch(setProfileLoading());
+  axios
+    .get("/api/profile/")
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        //THIS IS empty because , user may exist, but does not have a profile
+        type: GET_PROFILE,
+        payload: {}
+      })
+    );
+};
+
+//Profile loading, displays the little profile loading circle
+export const setProfileLoading = () => {
+  return {
+    type: PROFILE_LOADING
+  };
+};
+
+//Clear Profile
+export const clearCurrentProfile = () => {
+  return {
+    type: CLEAR_CURRENT_PROFILE
+  };
+};
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux"; //connecting it to redux
 import { logoutUser } from "../../actions/authActions";
+import { clearCurrentProfile } from "../../actions/profileActions";
 
 class Navbar extends Component {
   onLogoutClick(e) {
     //e is type of event
     e.preventDefault();
+    this.props.clearCurrentProfile();
     this.props.logoutUser();
   }
 
@@ -73,6 +75,11 @@ class Navbar extends Component {
                   Courses
                 </Link>
               </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/profiles">
+                  Profiles
+                </Link>
+              </li>
             </ul>
             {isAuthenticated ? authLinks : guestLinks}
           </div>
@@ -93,5 +100,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { logoutUser }
+  { logoutUser, clearCurrentProfile }
 )(Navbar);
